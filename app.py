3d2c197776436f271fb74b58f03dcc1afcba4d4d
afomike <temidayoafote@gmail.com>
2024-07-23 13:45:09 +0100

    done update

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+# app.secret_key = os.urandom(12).hex()
+app.config['SECRET_KEY'] = os.urandom(12).hex()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///attack_cycle.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
 
 # Define the models for each phase
 class InformationGathering(db.Model):
@@ -52,11 +64,52 @@ with app.app_context():
     db.create_all()
 
 # Define routes and view functions
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Hash the password
+        hashed_password = generate_password_hash(password, method='sha256')
+        
+        # Create a new user with the hashed password
+        new_user = User(username=username, password=hashed_password)
+        
+        db.session.add(new_user)
+        db.session.commit()
+        
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    
+    return render_template('register.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 # Home Route
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
 
 @app.route('/gather_info', methods=['GET', 'POST'])
 def gather_info():
